7336db19c12abfdcaf0007ed598155c41c2aaa69
Louislf <lefrancois_louis@yahoo.fr>
2025-02-17 17:32:40 +0100

    ex10

--- Exercice10/index.js
+++ Exercice10/index.js
@@ -1,3 +1,11 @@
 //votre code ici
 
-export default printNumbers
+function printNumbers(n) {
+  let result = "";
+  for (let i = 1; i <= n; i++) {
+    result = result + i + " ";
+  }
+  return result.trim();
+}
+
+export default printNumbers;
